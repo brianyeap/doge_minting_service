a4f5e4d89dbda12e70e6234f7f7ed42a93a4861e
Brian yeap <brianyeap@hotmail.com>
2023-03-03 03:09:19 +0800

    Update urls.py

--- django_project/main/urls.py
+++ django_project/main/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views as main_views
 
 urlpatterns = [
-    path('api/get_user_holdings/', main_views.api_create_wallet, name='api_create_wallet'),
+    path('api/create_user_wallet/', main_views.api_create_wallet, name='api_create_wallet'),
 ]
