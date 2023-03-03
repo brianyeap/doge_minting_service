4896bdc8781ff600d10b66e88240a7182473684c
Brian yeap <brianyeap@hotmail.com>
2023-03-04 05:48:04 +0800

    fs

--- django_project/main/urls.py
+++ django_project/main/urls.py
@@ -4,7 +4,7 @@ from . import views as main_views
 urlpatterns = [
     path('api/create_user_wallet/', main_views.api_create_wallet, name='api_create_wallet'),
     path('api/query_bal/', main_views.api_query_bal, name='api_query_bal'),
-    path('api/query_bal_no_wallet/', main_views.api_query_bal, name='api_query_bal_no_wallet'),
+    path('api/query_bal_no_wallet/', main_views.api_query_bal_no_wallet, name='api_query_bal_no_wallet'),
     path('api/mint_nft/', main_views.api_mint_nft, name='api_mint_nft'),
     path('api/send_funds/', main_views.api_send_funds, name='api_send_funds'),
     path('api/empty_wallet/', main_views.api_empty_wallet, name='api_empty_wallet'),
